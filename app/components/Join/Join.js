79064817abe94cf87cd571870ed2f0d0ee328bcc
Elite-tch <elite6461@gmail.com>
2025-01-01 01:12:20 +0100

    image

--- app/components/Join/Join.js
+++ app/components/Join/Join.js
@@ -15,29 +15,29 @@ export default function Join(){
 <div className="w-[90%] mx-auto"><h6>watch and listen</h6>
 <h2>the benefit of joining our church</h2>
 </div>
-<div className="flex justify-center items-center gap-8">
+<div className="flex justify-center items-center gap-8 w-[95%] md:w-[90%] mx-auto mt-8 md:flex-row  flex-col">
     <div className="relative ">
-        <Image src='https://res.cloudinary.com/dibwnfwk9/image/upload/v1735277248/fashion-man-love-people-5875438_znixx4.png' width={350} height={300} alt='' />
-        <div className="absolute bottom-0 left-0  text-white bg-black bg-opacity-50 px-2 pt-[80%] w-[100%] h-[100%]">
+        <img src='https://res.cloudinary.com/dibwnfwk9/image/upload/v1735277248/fashion-man-love-people-5875438_znixx4.png'   alt='' />
+        <div className="absolute bottom-0 left-0  text-white bg-black bg-opacity-50 px-4 pt-[90%] md:pt-[70%] w-[100%] h-[100%]">
             <h6 className="w-[90%] text-left">Watch and Listen to Our Sermons</h6>  
             <p className="w-[90%]">Watch and listen to sermons anytime, anywhere, at your convenience.</p>
         </div>
     </div>
     <div className="relative">
-        <Image src='https://res.cloudinary.com/dibwnfwk9/image/upload/v1735277238/a-statue-holding-a-holy-book-5613134_wlg6xo.png' width={350} height={300} alt='' />   
-        <div className="absolute bottom-0 left-0 text-white bg-black bg-opacity-50 px-2 pt-[100%] w-[100%] h-[100%]">
+        <img src='https://res.cloudinary.com/dibwnfwk9/image/upload/v1735277238/a-statue-holding-a-holy-book-5613134_wlg6xo.png'  alt='' />   
+        <div className="absolute bottom-0 left-0 text-white bg-black bg-opacity-50 px-4 pt-[100%] md:pt-[90%] w-[100%] h-[100%]">
             <h6 className="w-[90%] text-left"> Engage with Our Messages</h6>  
         </div>
     </div>
     <div className="relative">
-        <Image src='https://res.cloudinary.com/dibwnfwk9/image/upload/v1735277269/man-people-woman-connection-6276711_jctdc0.png' width={350} height={300} alt='' />  
-        <div className="absolute bottom-0 left-0 text-white bg-black bg-opacity-50 pt-[100%] px-2 w-[100%] h-[100%]">
+        <img src='https://res.cloudinary.com/dibwnfwk9/image/upload/v1735277269/man-people-woman-connection-6276711_jctdc0.png'  alt='' />  
+        <div className="absolute bottom-0 left-0 text-white bg-black bg-opacity-50 pt-[100%] md:pt-[90%] px-4 w-[100%] h-[100%]">
             <h6 className="w-[90%] text-left">Stay Connected to the Word </h6>     
         </div>
     </div>
     <div className="relative">
-        <Image src='https://res.cloudinary.com/dibwnfwk9/image/upload/v1735277294/woman-in-blue-tank-top-and-man-in-red-shirt-painting-3795022_fw8ml5.png'  width={350} height={300} alt='' />  
-        <div className="absolute bottom-0 left-0 text-white bg-black bg-opacity-50 pt-[100%] px-2 w-[100%] h-[100%]">
+        <img src='https://res.cloudinary.com/dibwnfwk9/image/upload/v1735277294/woman-in-blue-tank-top-and-man-in-red-shirt-painting-3795022_fw8ml5.png'  alt='' />  
+        <div className="absolute bottom-0 left-0 text-white bg-black bg-opacity-50 pt-[100%] md:pt-[95%] px-4 w-[100%] h-[100%]">
             <h6 className="w-[90%] text-left bottom-0">Explore Our Sermons</h6>    
         </div>
     </div>
